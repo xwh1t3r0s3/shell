1026d07d02ff00fe97a90c2fee646336847f9995
xwh1t3r0s3 <xwh1t3r0s3@users.noreply.github.com>
2016-10-24 00:36:11 +0900

    b374k.php got modified

--- b374k.php
+++ b374k.php
@@ -4,12 +4,13 @@
 	Jayalah Indonesiaku
 	(c)2013
 	http://code.google.com/p/b374k-shell
+	Recoded by wh1t3r0s3
 
 */
-$s_pass = "fb621f5060b9f65acf8eb4232e3024140dea2b34"; // default password : b374k (login and change to new password)
+$s_pass = "fb621f5060b9f65acf8eb4232e3024140dea2b34";
 
-$s_ver = "2.8"; // shell ver
-$s_title = "b374k ".$s_ver; // shell title
+$s_ver = "3.0"; // shell ver
+$s_title = "b374k recoded ".$s_ver;
 $s_login_time = 3600 * 24 * 7; // cookie time (login)
 $s_debug = false; // debugging mode
 
